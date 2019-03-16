0e41b661574f3c8e5b3d9506f00790951f32af57
yueyechen <bingdianyixia963@126.com>
2019-03-16 17:08:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,6 @@ def get_config(training = True):
     conf.eval = edict()
 
     conf.data_folder = 'path to data root' #data root for training, and testing, you should change is according with your setting
-#     conf.result_path = '/home2/xuejiachen/PAD/work_space/result'
-#     conf.log_path = '/home2/xuejiachen/PAD/work_space/log' #path for saving loggers in training process
     conf.save_path = './work_space/save' #path for save model in training process, you should change it according with your setting
     conf.train_list =  'quarter_face_train_list.txt' #path where training list is, you should change it according with your setting
     conf.test_list = 'test_public_list.txt' #path where test list is, you should change it according with your setting
